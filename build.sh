7d57d561d88c653fffb5c25e8b448124571d5537
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-10-31 16:09:47 +0900

    余計なファイルを含めないようにした

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 rm -rf dist
 
 echo "Building TypeScript sources..."
@@ -8,5 +7,6 @@ npx tsc -P ./tsconfig.json
 echo "Installing dependencies..."
 cp package.json dist
 cd dist
+rm -f ./package.json ./package-lock.json
 npm install --only=production
 zip -R "../dist.zip" "./**/*"
